2014b501165be0a63548b1bb4ebc6b26ddb90aad
matt-kendall-tessella <Matthew.Kendall@Tessella.com>
2014-05-23 19:07:00 +0100

    FlightControl setup

--- FlightControl/FlightControl.ino
+++ FlightControl/FlightControl.ino
@@ -1,13 +1,18 @@
+int ledPin = 13;
+
 void setup()
 {
-
+	pinMode(ledPin, OUTPUT);
   /* add setup code here */
 
 }
 
 void loop()
 {
-
+	digitalWrite(ledPin, LOW);
+	delay(100);
+	digitalWrite(ledPin, HIGH);
+	delay(500);
   /* add main program code here */
 
 }
